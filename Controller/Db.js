f95a08694e0a7ce02ba9eb848690a8c5a9737064
elisomorovskimedeiros <eli.sm@hotmail.com>
2019-06-15 12:01:46 -0300

    teste com novo usuário no mysql

--- Controller/Db.js
+++ Controller/Db.js
@@ -7,7 +7,7 @@ class Db{
     constructor(){
         this.connection = mysql.createConnection({
             host     : 'mysql10-farm76.kinghost.net',
-            user     : 'solevento_add1',
+            user     : 'solevento',
             password : 'Medeiros15',
             database : 'solevento', //não colocar se for criar um banco através do node
             //multipleStatements: true //cuidado: deve ser falso (padrão) para evitar sql injection - com ele true testar a rota: http://localhost:3000/post/1;DROP%20TABLE%20posts
