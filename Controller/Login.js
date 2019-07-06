eeb0cf6d18ca18a725c6e54905e89a1431b4307f
elisomorovskimedeiros <eli.sm@hotmail.com>
2019-07-06 20:37:14 -0300

    css na tela de login

--- Controller/Login.js
+++ Controller/Login.js
@@ -38,18 +38,18 @@ let login = function(passport){
         passReqToCallback: true //passback entire req to call back
     } , function (req, username, password, done){
 
-            if(!username || !password ) { return done(null, false, req.flash('message','All fields are required.')); }
+            if(!username || !password ) { return done(null, false, req.flash('message','Preencher todos os campos.')); }
             var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
             connection.query("select * from usuario where username = ?", [username], function(err, rows){
                 console.log(err); 
                 console.log(rows);
             if (err) return done(req.flash('message',err));
-            if(!rows.length){ return done(null, false, req.flash('message','Invalid username or password.')); }
+            if(!rows.length){ return done(null, false, req.flash('message','Usuário ou senha inválidos.')); }
             salt = salt+''+password;
             var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
             var dbPassword  = rows[0].password;
             if(!(dbPassword == encPassword)){
-                return done(null, false, req.flash('message','Invalid username or password.'));
+                return done(null, false, req.flash('message','Usuário ou senha inválidos.'));
             }
             return done(null, rows[0]);
             });
