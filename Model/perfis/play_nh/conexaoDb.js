49b30f69e8657d1713d6a084aaeb91295d98faba
elisomorovskimedeiros <eli.sm@hotmail.com>
2019-08-30 11:41:56 -0300

    db da playnh liberado para uso

--- Model/perfis/play_nh/conexaoDb.js
+++ Model/perfis/play_nh/conexaoDb.js
@@ -1,7 +1,7 @@
 const connection = ({
     host     : 'mysql10-farm76.kinghost.net',
-    user     : 'solevento_add01',
-    password : 'Medeiros15',
+    user     : 'solevento01',
+    password : 'playnh01',
     database : 'solevento01', //não colocar se for criar um banco através do node
     multipleStatements: true //cuidado: deve ser falso (padrão) para evitar sql injection - com ele true testar a rota: http://localhost:3000/post/1;DROP%20TABLE%20posts
 });
