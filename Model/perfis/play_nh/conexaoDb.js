ad5c493e293d82f66eb39247c085ef3b25f9fa20
elisomorovskimedeiros <eli.sm@hotmail.com>
2019-09-17 17:29:48 -0300

    esqueci de destrocar os bancos de dados

--- Model/perfis/play_nh/conexaoDb.js
+++ Model/perfis/play_nh/conexaoDb.js
@@ -1,4 +1,4 @@
-/*
+
 const connection = ({
     host     : 'mysql10-farm76.kinghost.net',
     user     : 'solevento01',
@@ -6,7 +6,7 @@ const connection = ({
     database : 'solevento01', //não colocar se for criar um banco através do node
     multipleStatements: true //cuidado: deve ser falso (padrão) para evitar sql injection - com ele true testar a rota: http://localhost:3000/post/1;DROP%20TABLE%20posts
 });
-*/
+/*
 const connection = ({
     host     : 'localhost',
     user     : 'play',
@@ -14,5 +14,5 @@ const connection = ({
     database : 'play', //não colocar se for criar um banco através do node
     multipleStatements: true //cuidado: deve ser falso (padrão) para evitar sql injection - com ele true testar a rota: http://localhost:3000/post/1;DROP%20TABLE%20posts
 });
-
+*/
 module.exports = connection;
